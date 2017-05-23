a0f226dee239821917550383fe9ced9a337ac638
Eivind Fonn <evfonn@gmail.com>
2017-05-23 13:52:15 +0200

    Comment ramos/utils/parallel

--- ramos/utils/parallel/__init__.py
+++ ramos/utils/parallel/__init__.py
@@ -8,11 +8,23 @@ __all__ = ['parmap']
 
 
 def split(lst, n):
+    """Split a list `lst` into n blocks, as equal as possible in size."""
     k, m = divmod(len(lst), n)
     return [lst[i*k+min(i,m):(i+1)*k+min(i+1,m)] for i in range(n)]
 
 
 def delegate(pid, q, target, chunk, args, reduction):
+    """Worker function for use in parmap.
+
+    - `pid`: The ID of this worker process
+    - `q`: Queue to which result will be sent
+    - `target`: The worker function that will actually do the work
+    - `chunk`: List of argument tuples
+    - `args`: Tuple of constant arguments
+    - `reduction`: Function to reduce the result, or None
+
+    Will put the tuple (pid, <result>) to the queue `q`.
+    """
     result = []
     for c in chunk:
         cur_args = list(c) + list(args)
@@ -25,13 +37,28 @@ def delegate(pid, q, target, chunk, args, reduction):
 
 
 def parmap(target, varying, constant=(), reduction=None, ncpus=None, unwrap=True):
+    """Parallel map
+
+    - `target`: a function to be called on all inputs
+    - `varying`: a list of tuples of arguments to pass to the target function
+    - `constant`: a tuple of arguments to pass to the target function
+    - `reduction`: optional function to reduce the output
+    - `ncpus`: optionally, number of parallel workers to use
+    - `unwrap`: if false, treat `varying` as a list of single arguments, rather
+      than as a list of argument tuples
+
+    The target function must be written so that the variable arguments come
+    before the constant ones.
+    """
     if not ncpus:
         ncpus = os.cpu_count()
     if not unwrap:
         varying = [(v,) for v in varying]
-    chunks = split(varying, ncpus)
-    q = Queue()
 
+    chunks = split(varying, ncpus)
+    q = Queue()                 # Return values will be read from the queue
+
+    # Start a process for each worker, and run the delegate function
     processes = []
     for pid, chunk in enumerate(chunks):
         p = Process(
@@ -41,13 +68,16 @@ def parmap(target, varying, constant=(), reduction=None, ncpus=None, unwrap=True
         p.start()
         processes.append((pid, p))
 
+    # Grab the results from each worker and shut them down
     result = [q.get() for _ in range(ncpus)]
 
     for pid, p in processes:
         p.join()
 
+    # Sort results by pid
     result = [v for _, v in sorted(result, key=itemgetter(0))]
 
+    # Return either reduction or flattened list
     if reduction:
         return reduction(result)
     return list(chain.from_iterable(result))
--- ramos/utils/parallel/workers.py
+++ ramos/utils/parallel/workers.py
@@ -1,25 +1,47 @@
+"""Collection of commonly used parallel worker functions."""
+
 import numpy as np
 
 
 def energy_content(source, level, field, mx):
+    """Compute the energy content of a field.
+
+    - `source`: the data source
+    - `level`: the time level to read from
+    - `field`: the name of the field to read
+    - `mx`: mass matrix
+
+    Equivalent of u^T × M × u, where M is the mass matrix and U is the
+    coefficient vector.
+    """
     coeffs = source.coefficients([field], level)
     return mx.dot(coeffs).dot(coeffs)
 
 
 def normalized_coeffs(source, level, fields, mass):
+    """Returns the coefficient vector of a number of fields, centered around
+    the component-wise mean.
+
+    - `source`: the data source
+    - `level`: the time level to read from
+    - `fields`: the name of the fields to read
+    - `mx`: dict mapping field names to mass matrices
+    """
     ret = []
     for field in fields:
         mx = mass[field]
         coeffs = source.coefficients(field, level, flatten=False)
-        mean = np.sum(mx.dot(coeffs), axis=0) / mx.sum()
-        coeffs -= mean
-        ret.append(np.ndarray.flatten(coeffs))
+        mean = np.sum(mx.dot(coeffs), axis=0) / mx.sum()  # Compute component-wise means,
+        coeffs -= mean                                    # and subtract them.
+        ret.append(np.ndarray.flatten(coeffs))            # The result needs to be flattened.
     return np.hstack(coeffs)
 
 
 def mv_dot(vec, mx):
+    """Computes a matrix-vector product."""
     return mx.dot(vec)
 
 
 def vv_dot(va, vb):
+    """Computes a vector-vector dot product."""
     return va.dot(vb)
