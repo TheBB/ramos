6692b4dd266e76af172914165de213b0d406e5f4
Eivind Fonn <evfonn@gmail.com>
2017-05-23 13:24:41 +0200

    Comment VTKFilesSource

--- ramos/io/VTKFiles.py
+++ ramos/io/VTKFiles.py
@@ -11,8 +11,19 @@ from ramos.utils.vtk import mass_matrix, write_to_file
 class VTKFilesSource(DataSource):
 
     def __init__(self, files):
+        """VTKFilesSource reads this type of structure:
+
+        <basename>-0.vtk
+        <basename>-1.vtk
+        <basename>-2.vtk
+        ...
+        <basename>-n.vtk
+        """
         self.files = files
 
+        # Try to figure out how many parametric dimensions this data has.
+        # Do this by loading any dataset and inspecting its bounding box.
+        # (Not foolproof.)
         dataset = self.dataset(0)
         xmin, xmax, ymin, ymax, zmin, zmax = dataset.GetBounds()
         variates = [
@@ -20,9 +31,16 @@ class VTKFilesSource(DataSource):
             for a, b in ((xmin,xmax), (ymin,ymax), (zmin,zmax))
         ]
         pardim = sum(variates)
+
+        # Call superclass constructor
         super(VTKFilesSource, self).__init__(pardim, len(files))
+
+        # List of physical dimensions that are also parametric dimensions
         self.variates = [i for i, v in enumerate(variates) if v]
 
+        # Discover all the fields, by inspecting the files in the first time
+        # level. No checking is done to make sure this info is consistent with
+        # other time levels.
         pointdata = dataset.GetPointData()
         for i in range(pointdata.GetNumberOfArrays()):
             name = pointdata.GetArrayName(i)
@@ -31,24 +49,36 @@ class VTKFilesSource(DataSource):
             self.add_field(name, ncomps, size)
 
     def datasets():
+        """Iterate over all datasets in this source."""
         return ((i, self.dataset(i)) for i in range(len(self.files)))
 
     def dataset(self, index):
+        """Return a single dataset associated with a file index."""
         reader = vtkDataSetReader()
         reader.SetFileName(self.files[index])
         reader.Update()
         return reader.GetOutput()
 
     def field_mass_matrix(self, field):
+        """Return the mass matrix for a single field."""
+
+        # See ramos.utils.vtk.mass_matrix for more info
         return mass_matrix(self.dataset(0), self.variates)
 
     def field_coefficients(self, field, level=0):
+        """Return the coefficient vector for a single field at a given time level."""
         dataset = self.dataset(level)
         pointdata = dataset.GetPointData()
         array = pointdata.GetAbstractArray(field.name)
         return vtk_to_numpy(array)
 
     def tesselate(self, field, level=0):
+        """Return a tesselation (for plotting) of a single field at a given time level.
+
+        Returns either:
+        - (x, y, cell_indices), coeffs  (if all cells are triangles)
+        - (x, y), coeffs                (if there are higher order cells)
+        """
         field = self.field(field)
         dataset = self.dataset(level)
         points = vtk_to_numpy(dataset.GetPoints().GetData())
@@ -61,6 +91,7 @@ class VTKFilesSource(DataSource):
         return (x, y, cell_indices), coeffs
 
     def sink(self, *args, **kwargs):
+        """Create a data sink (an output class) that matches the source."""
         return VTKFilesSink(self, *args, **kwargs)
 
 
